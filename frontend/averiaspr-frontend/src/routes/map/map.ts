b2d74b8c78c1a7069d1acaac38554d965c6037a4
Gio-Benitez <giobenitez66@gmail.com>
2024-03-08 19:44:31 -0400

    Added more map layout options

--- frontend/averiaspr-frontend/src/routes/map/map.ts
+++ frontend/averiaspr-frontend/src/routes/map/map.ts
@@ -6,3 +6,9 @@ var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/a
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	ext: 'png'
 });
+
+//Leaflet map layer for Esri gray map
+var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+});
