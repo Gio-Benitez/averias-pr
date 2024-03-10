ba2c3bc5d4a10472e33beb0e43e0f615e7af14ed
Gio-Benitez <giobenitez66@gmail.com>
2024-03-09 20:48:13 -0400

    Adding .vscode config

---
+++ frontend/averiaspr-frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import type { PageData } from './$lib/types';
+    
+    export let data: PageData;
+</script>
--- frontend/averiaspr-frontend/src/routes/map/map.ts
+++ frontend/averiaspr-frontend/src/routes/map/map.ts
@@ -1,6 +1,6 @@
 
 // Leaflet map layer for Smooth Dark map
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+let Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
 	minZoom: 0,
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -8,7 +8,7 @@ var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/a
 });
 
 //Leaflet map layer for Esri gray map
-var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
 	maxZoom: 16
 });
