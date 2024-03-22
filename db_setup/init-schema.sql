0ce346a3c02f967374290c583a74ff812ac03d73
GregAlexander07 <greg.viera@upr.edu>
2024-03-21 20:19:00 -0400

    The app folder was moved to the db_setup file Restructured the docker file: coy requirements.txt installation before any copy of app refactor the requirement.txt to requirements.txt changed content of requirements.txt
    Know issue:
    when running the container its suppose to initialize the db tables,

--- db_setup/init-schema.sql
+++ db_setup/init-schema.sql
@@ -1,10 +1,33 @@
-CREATE USER appuser WITH PASSWORD 'appass';
-CREATE DATABASE averiaspr_db;
-GRANT ALL PRIVILEGES ON DATABASE averiaspr_db TO appuser;
+-- CREATE USER appuser WITH PASSWORD 'appass';
+-- CREATE DATABASE averiaspr_db;
+-- GRANT ALL PRIVILEGES ON DATABASE averiaspr_db TO appuser;
+--
+-- \c averiaspr_db;
 
-\c averiaspr_db;
+-- CREATE TABLE IF NOT EXISTS my_table (
+--     client_id character varying(36) NOT NULL,
+--     value character varying(255)
+-- );
+--
 
-CREATE TABLE IF NOT EXISTS my_table (
-    client_id character varying(36) NOT NULL,
-    value character varying(255)
+CREATE TABLE "user" (
+    user_id SERIAL PRIMARY KEY,
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    user_pass_hash VARCHAR(255) NOT NULL,
+    user_salt VARCHAR(255) NOT NULL,
+    user_fname VARCHAR(100),
+    user_lname VARCHAR(100)
+);
+
+CREATE TABLE report_data (
+    data_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES "user"(user_id),
+    Address_Line_1 VARCHAR(255),
+    Address_Line_2 VARCHAR(255),
+    Report_category VARCHAR(100),
+    City_name VARCHAR(100),
+    Zipcode VARCHAR(20),
+    Geo_Data_Lat DECIMAL(10, 6),
+    Geo_Data_Long DECIMAL(10, 6),
+    image_src TEXT
 );
