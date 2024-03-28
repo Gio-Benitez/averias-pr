2a228b5ad779c49d47e26c5fdf128282c8bedd3d
wester-is-not-real <wester.aldarondo@upr.edu>
2024-03-28 11:06:01 -0400

    Theme button is now functional

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -6,28 +6,19 @@
       <a href="/map" class="flex items-center bg-slate ml-8 mr-2">Map</a>
       <a href="/dashboard" class="flex items-center bg-slate ml-8 mr-2">Dashboard</a>
       <a href="/report" class="flex items-center bg-slate ml-8 mr-2" style="white-space: nowrap;"> Create a Report</a>
-      <section class="dropdown dropdown-end dropdown-bottom w-full py-4 pr-8 ml-8 mr-2">
-        <button>Theme</button>
-        <ul tabIndex={1} class="menu dropdown-content rounded-box z-50 w-52 bg-base-100 p-2 shadow">
-        <li>
-          <option data-set-theme="light">Light</option>
-        </li>
-        <li>
-          <option data-set-theme="dark">Dark</option>
-        </li>
-        <li>
-          <option data-set-theme="cupcake">Cupcake</option>
-        </li>
-        <li>
-          <option data-set-theme="retro">Retro</option>
-        </li>
-        <li>
-          <option data-set-theme="corporate">Corporate</option>
-        </li>
-        <li>
-          <option data-set-theme="business">Business</option>
-        </li>
-      </ul>
+      <section class="dropdown w-full py-4 pr-8 ml-8 mr-2">
+        <!-- <button class="btn btn-outline btn-primary">Theme</button> -->
+        <div tabindex="0" role="button" class="btn m-1 btn-outline">
+          Theme
+          <svg width="12px" height="12px" class="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
+        </div>
+        <ul tabindex="0" class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+          <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Default" value="default"/></li>
+          <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Retro" value="retro"/></li>
+          <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Cyberpunk" value="cyberpunk"/></li>
+          <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Valentine" value="valentine"/></li>
+          <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Aqua" value="aqua"/></li>
+        </ul>
       </section>
     </section>
     <a href="/account" class="flex items-center bg-slate mr-5">Sign In</a>
