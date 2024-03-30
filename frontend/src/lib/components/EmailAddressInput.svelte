cdbefeb104e752243809bcd7061a41cf13cd621f
Gio-Benitez <giobenitez66@gmail.com>
2024-03-30 19:53:25 -0400

    Created Email Address input field component

---
+++ frontend/src/lib/components/EmailAddressInput.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+
+<input type="email" placeholder="Email Address"/>
+
+
+<style>
+	input {
+		min-width: 15rem;
+		padding: 0.5rem;
+		border: 1px solid #ccc;
+		border-radius: 0.25rem;
+	}
+</style>
