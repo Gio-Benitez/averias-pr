89332676ad7bf84951fcacdd7681d0b5ee12d62b
Gio-Benitez <giobenitez66@gmail.com>
2024-04-04 17:43:17 -0400

    Refactored root layout.svelte to match main branch

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import Footer from '$components/Footer.svelte';
   import NavBar from '$components/NavBar.svelte';
-  import NavBarAlt from '$components/NavBarAlt.svelte';
   import { onMount } from 'svelte';
   import { themeChange } from 'theme-change';
   import '../app.postcss';
-  import { AppShell } from '@skeletonlabs/skeleton';
 
   // initialize theme change
   onMount(() => {
