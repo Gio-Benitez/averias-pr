0733de33c309c533eb17259fcdb1f7877d078184
wester-is-not-real <wester.aldarondo@upr.edu>
2024-04-12 16:00:06 -0400

    Added location tracking service, and detect location button in condensed create report step bar.

--- frontend/src/lib/components/CreateReportCondensed.svelte
+++ frontend/src/lib/components/CreateReportCondensed.svelte
@@ -1,27 +1,82 @@
-<script>
+<script lang="ts">
   import target_icon from '$lib/images/target_icon.png';
-  let steps_counter = 0;
+  import { getUserLocation } from "$lib/geolocation";
+  import { DefaultMarker, MapLibre, Popup} from 'svelte-maplibre'
+  // import { LngLat } from 'maplibre-gl';
+
+  // Define type for GeoJSON Point
+  interface GeoJSONPoint {
+    type: string;
+    coordinates: [number, number];
+  }
+
+  let userLocation: GeoJSONPoint | null = null;
+
+  let errorMessage: string | null = null;
+
+  async function fetchUserLocation() {
+    try {
+      userLocation = await getUserLocation();
+      errorMessage = null; // Reset error message on success
+      // let ll = new LngLat(userLocation?.coordinates[0],userLocation?.coordinates[1]);
+    } catch (error) {
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      } else {
+        errorMessage = "An unknown error occurred.";
+      }
+      console.error(errorMessage);
+    }
+  }
+  
+  
+  let steps_counter = 1;
+    
+
 </script>
 <div>
   <!-- If steps_counter > 0, display steps -->
   {#if steps_counter>0}
-    <div class="flex justify-items-center flex-col align-middle mt-5">
+    <!-- <div class="flex flex-col mt-5"> -->
       {#if steps_counter===1}
-          <ul class="steps sm:steps-vertical lg:steps-horizontal">
+        <div class="flex justify-center w-full">
+          <ul class="steps sm:steps-vertical lg:steps-horizontal w-full" style="z-index:-1;">
             <li class="step step-primary text-sm">Detect Location</li>
             <li class="step text-sm">Select Category</li>
             <li class="step text-sm">Upload Photo</li>
             <li class="step text-sm">Create Report</li>
           </ul>
-        <div role="button" class="btn btn-xm btn-ghost m-1 btn-circle ">
-          <div class="w-5 h-5 rounded-full overflow-hidden">
-            <img alt="Theme Icon" src="{target_icon}" />
-          </div>
         </div>
+        <button class="btn btn-lg btn-square btn-primary m-1 mt-20" on:click={fetchUserLocation}>
+          <div class="w-20 h-20 overflow-hidden">
+            <img alt="Theme Icon" src="{target_icon}" />              
+          </div>
+        </button>
+        <span>Press to detect location</span>
+        {#if userLocation}
+          <div>
+            <MapLibre
+              center={[userLocation ? userLocation.coordinates[0] : 0, userLocation ? userLocation.coordinates[1] : 0]}
+              zoom={14}
+              class="map"
+              style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+              maxBounds={[-67.5, 17.8, -65.1, 18.6]}
+            >
+              <!-- <DefaultMarker {ll} draggable>
+                <Popup offset={[0, -10]}>
+                  <div class="text-lg font-bold">{"You are here"}</div>
+                </Popup>
+              </DefaultMarker> -->
+            </MapLibre>
+          </div>
+          <pre>{JSON.stringify(userLocation, null, 2)}</pre>
+        {:else}
+          <p>No location available</p>
+        {/if}
       {/if}
       
       {#if steps_counter===2}
-          <ul class="steps sm:steps-vertical lg:steps-horizontal">
+          <ul class="steps sm:steps-vertical lg:steps-horizontal" style="z-index:-1;">
             <li class="step step-primary text-sm">Detect Location</li>
             <li class="step step-primary text-sm">Select Category</li>
             <li class="step text-sm">Upload Photo</li>
@@ -30,7 +85,7 @@
       {/if}
 
       {#if steps_counter===3}
-          <ul class="steps sm:steps-vertical lg:steps-horizontal">
+          <ul class="steps sm:steps-vertical lg:steps-horizontal" style="z-index:-1;">
             <li class="step step-primary text-sm">Detect Location</li>
             <li class="step step-primary text-sm">Select Category</li>
             <li class="step step-primary text-sm">Upload Photo</li>
@@ -38,14 +93,14 @@
           </ul>
       {/if}
       {#if steps_counter>=4}
-          <ul class="steps sm:steps-vertical lg:steps-horizontal">
+          <ul class="steps sm:steps-vertical lg:steps-horizontal" style="z-index:-1;">
             <li class="step step-primary text-sm">Detect Location</li>
             <li class="step step-primary text-sm">Select Category</li>
             <li class="step step-primary text-sm">Upload Photo</li>
             <li class="step step-success text-sm text-success">Create Report</li>
           </ul>
       {/if}
-    </div>
+    <!-- </div> -->
   {/if}
   <!-- Steps Buttons -->
   <div class="mt-5">
@@ -73,5 +128,9 @@
     flex-direction: column; /* Stack items vertically */
     /* justify-content: center; /* Align items vertically in the center */
     /*align-items: center; Align items horizontally in the center */
+  }
+  :global(.map) {
+    height: 200px;
+    width: 35%;
   }
 </style>
