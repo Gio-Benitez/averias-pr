bc7ed6228d4a837fa25bdc16cfd1c606e1d9f31f
Gio-Benitez <giobenitez66@gmail.com>
2024-04-18 15:12:39 -0400

    Added stat bubbles for additonal statistics, changed re-sizing behavior of data panel components to keep fit consistency

--- frontend/src/lib/components/MapDataPane.svelte
+++ frontend/src/lib/components/MapDataPane.svelte
@@ -2,10 +2,10 @@
     import mapDataStore from "$lib/stores";
 </script>
 
-<div class="card flex-grow flex-shrink w-full h-full font-Alpino text-secondary-content bg-secondary shadow-lg " >
+<div class="card grow-0 shrink-0 min-w-full min-h-full font-Alpino text-secondary-content bg-secondary shadow-lg " >
     <div class="card-body">
         <h1 class="card-title justify-center">{$mapDataStore.dataRegion}</h1>
-        <div class="flex flex-nowrap space-x-4">
+        <div class="grid min-w-full  grid-col-2 grid-row-5 grid-flow-row gap-y-4">
             <div class="SmallCard">
                 <p># of Reports</p>
                 <h1>{$mapDataStore.numOfReports}</h1>
@@ -13,21 +13,29 @@
             <div class="SmallCard">
                 <p>Population</p>
                 <h1>{$mapDataStore.populationData}</h1>
-            </div>      
+            </div>
+            <div class="SmallCard">
+                <p>Stat 1</p>
+                <h1>{$mapDataStore.stat1}</h1>
+            </div> 
+            <div class="SmallCard">
+                <p>Stat 2</p>
+                <h1>{$mapDataStore.stat2}</h1>
+            </div>     
         </div>
     </div>
 </div>
 
 <style lang="postcss">
     .SmallCard {
-        @apply flex flex-wrap bg-primary rounded shadow px-2 py-4 w-full h-full text-center justify-center text-secondary-content;
+        @apply flex flex-wrap col-span-1 row-span-1 bg-primary rounded shadow px-2 py-4 w-full h-full text-secondary-content;
     }
 
     h1 {
-        @apply font-black text-3xl;
+        @apply font-black text-3xl text-right align-middle;
     }
     p {
-        @apply font-medium text-lg;
+        @apply font-medium text-lg text-left align-middle;
     }
     
 </style>
