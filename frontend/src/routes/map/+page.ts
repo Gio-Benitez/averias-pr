0ab35737ce2efe60a433c250b698d8bf04bbc0ce
Gio-Benitez <giobenitez66@gmail.com>
2024-04-17 22:03:37 -0400

    Created load function to load population data from Census API. Only islandwide population supported.

---
+++ frontend/src/routes/map/+page.ts
@@ -0,0 +1,6 @@
+import type { PageLoad } from './$types';
+export const load: PageLoad = async ({ fetch }) => {	
+    const res = await fetch('https://api.census.gov/data/2020/dec/cd118?get=NAME,H8_001N&for=state:72');	
+    const item = await res.json();
+    console.log(item)
+	return { item };};
