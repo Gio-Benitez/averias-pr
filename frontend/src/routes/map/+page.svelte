d7bad24c87b5a0896f14964f70b884f6fe8cc795
Gio-Benitez <giobenitez66@gmail.com>
2024-04-17 22:03:29 -0400

    Added Map Data Panel to Map page, styled Map and Panel to show up side by side

--- frontend/src/routes/map/+page.svelte
+++ frontend/src/routes/map/+page.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+	import MapDataPane from '$components/MapDataPane.svelte';
     import Map from '$components/Map.svelte';
-    let name = 'world';
+    import mapDataStore from '$lib/stores';
+
+    export let data: PageData;
+    $mapDataStore.populationData = data.item[1][1];
+
   </script>
 <svelte:head>
     <!--- To-Do -->
 </svelte:head>
 <!--Interactive Map Segment-->
-<main class="flex h-full flex-col gap-8 pb-8 text-center object-center">
+<main class="grid grid-rows-9 grid-cols-10 grid-flow-auto h-full gap-8 pb-8">
 
-    <h1>Interactive Map</h1>
-    <div id="map" class="flex  h-2/3 w-3/4 justify-center content-center">
+    <h1 class="col-span-1 row-span-1 col-start-5 col-end-6 row-start-1">Interactive Map</h1>
+    <div id="map" class="col-start-2 col-end-8 row-start-2 row-end-9">
         <Map />
+    </div>
+    <div id="dataPane" class="flex aspect-auto col-span-2 col-start-8 row-start-2 row-end-9">
+        <MapDataPane />
     </div>
 
 </main>
