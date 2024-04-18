d09202c3652630d4f3ce14cdd35246175867c34d
Gio-Benitez <giobenitez66@gmail.com>
2024-04-17 21:59:51 -0400

    Created svelte store to populate map data panel

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -6,3 +6,15 @@ export let isSignInModalOpen = writable(false);
 export let isCreateAccountModalOpen = writable(false);
 export let isForgotPasswordModalOpen = writable(false);
 export let invalidAuth = writable(false);
+
+// Interactive Map Stores
+// Initialize Map data panel with default values (Stats for all of Puerto Rico)
+let mapDataStore = writable({
+    dataRegion: 'Puerto Rico',
+    numOfReports: 100,
+    populationData: 0,
+    stat1: 0,
+    stat2: 0
+});
+
+export default mapDataStore;
