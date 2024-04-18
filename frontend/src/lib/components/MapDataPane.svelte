f30549cd7c3327b99dfc07124c9230d8e01e6d2f
Gio-Benitez <giobenitez66@gmail.com>
2024-04-17 22:27:14 -0400

    Updated data pane to change colors with theme selector

--- frontend/src/lib/components/MapDataPane.svelte
+++ frontend/src/lib/components/MapDataPane.svelte
@@ -2,7 +2,7 @@
     import mapDataStore from "$lib/stores";
 </script>
 
-<div class="card flex-grow flex-shrink w-full h-full font-Alpino text-black bg-slate-200  shadow-lg">
+<div class="card flex-grow flex-shrink w-full h-full font-Alpino text-secondary-content bg-secondary shadow-lg " >
     <div class="card-body">
         <h1 class="card-title justify-center">{$mapDataStore.dataRegion}</h1>
         <div class="flex flex-nowrap space-x-4">
@@ -20,7 +20,7 @@
 
 <style lang="postcss">
     .SmallCard {
-        @apply flex flex-wrap bg-secondary rounded shadow px-2 py-4 w-full h-full text-center justify-center text-secondary-content;
+        @apply flex flex-wrap bg-primary rounded shadow px-2 py-4 w-full h-full text-center justify-center text-secondary-content;
     }
 
     h1 {
