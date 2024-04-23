56022ce8b0f99808c11883eaceee1438a32f2319
Gio-Benitez <giobenitez66@gmail.com>
2024-04-22 22:23:20 -0400

    Created dashboard panel component (Currently placeholder background)

---
+++ frontend/src/lib/components/DashboardPanel.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { dashboardStore } from "$lib/stores";
+</script>
+
+<div class="rounded-2xl aspect-auto w-full h-full bg-slate-800 shadow-lg">
+
+</div>
+
+
+<style lang="postcss">
+
+</style>
