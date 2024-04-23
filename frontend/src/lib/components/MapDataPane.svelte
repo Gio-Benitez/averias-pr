00af69cbbcbdd623cc33b738a1a785cfaed6e399
Gio-Benitez <giobenitez66@gmail.com>
2024-04-22 22:23:48 -0400

    Created map data panel with filters and statistics

--- frontend/src/lib/components/MapDataPane.svelte
+++ frontend/src/lib/components/MapDataPane.svelte
@@ -1,41 +1,59 @@
 <script lang="ts">
-    import mapDataStore from "$lib/stores";
+    import { mapDataStore } from "$lib/stores";
 </script>
-
-<div class="card grow-0 shrink-0 min-w-full min-h-full font-Alpino text-secondary-content bg-secondary shadow-lg " >
-    <div class="card-body">
-        <h1 class="card-title justify-center">{$mapDataStore.dataRegion}</h1>
-        <div class="grid min-w-full  grid-col-2 grid-row-5 grid-flow-row gap-y-4">
-            <div class="SmallCard">
-                <p># of Reports</p>
-                <h1>{$mapDataStore.numOfReports}</h1>
-            </div>
-            <div class="SmallCard">
-                <p>Population</p>
-                <h1>{$mapDataStore.populationData}</h1>
-            </div>
-            <div class="SmallCard">
-                <p>Stat 1</p>
-                <h1>{$mapDataStore.stat1}</h1>
-            </div> 
-            <div class="SmallCard">
-                <p>Stat 2</p>
-                <h1>{$mapDataStore.stat2}</h1>
-            </div>     
+<div class="flex flex-col flex-wrap w-full h-full bg-primary px-8 py-12 rounded-2xl">
+    <div class="flex flex-col w-full h-2/5 text-center gap-2">
+        <h1>Filters</h1>
+        <div class="dropdown">
+            <div tabindex="0" role="button" class="btn m-1 w-full">Select Region</div>
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full">
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <div tabindex="0" role="button" class="btn m-1 w-full">Select Report Category</div>
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full">
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
         </div>
     </div>
+    <div class="flex flex-col w-full h-1/5 text-center">
+        <h1>Statistics</h1>
+        <div class="divider divider-neutral"></div>
+    </div>
+    <div class="flex flex-col flex-wrap w-full h-2/5 text-center gap-2">
+        <div class="stat w-1/2 shadow-lg">
+            <div class="stat-title"># of Reports</div>
+            <div class="stat-value">{$mapDataStore.numOfReports}</div>
+        </div>
+        <div class="stat w-1/2 shadow-lg">
+            <div class="stat-title">Population</div>
+            <div class="stat-value">{$mapDataStore.populationData}</div>
+        </div>
+        <div class="stat w-1/2 shadow-lg">
+            <div class="stat-title">Stat 1</div>
+            <div class="stat-value">{$mapDataStore.stat1}</div>
+        </div> 
+        <div class="stat w-1/2 shadow-lg">
+            <div class="stat-title">Stat 2</div>
+            <div class="stat-value">{$mapDataStore.stat2}</div>
+        </div>
+    </div>
+    
 </div>
 
 <style lang="postcss">
-    .SmallCard {
-        @apply flex flex-wrap col-span-1 row-span-1 bg-primary rounded shadow px-2 py-4 w-full h-full text-secondary-content;
-    }
-
     h1 {
-        @apply font-black text-3xl text-right align-middle;
+        @apply text-primary-content text-3xl font-bold;
     }
-    p {
-        @apply font-medium text-lg text-left align-middle;
+    .stat-title {
+        @apply text-primary-content text-lg;
     }
-    
+    .stat-value {
+        @apply text-primary-content text-2xl;
+    }
 </style>
