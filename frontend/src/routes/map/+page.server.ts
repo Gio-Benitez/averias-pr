2f383585433b1400d058ee00ac6ae53c52cd51c5
Gio-Benitez <giobenitez66@gmail.com>
2024-04-23 20:29:33 -0400

    Added documentation to guide server functions to be used for map page

---
+++ frontend/src/routes/map/+page.server.ts
@@ -0,0 +1,17 @@
+/* 
+Server-side functions for Interactive Map and Dashboard page, all requests to the data server are handled here.
+Functions here are used to fetch the statistics data corresponding to the selected filters in the Map or Dashboard. 
+*/
+
+// Imports
+
+// Map Data Stores
+
+// Dashboard Data Stores
+
+
+
+// Map Data Fetching
+
+
+// Dashboard Data Fetching
