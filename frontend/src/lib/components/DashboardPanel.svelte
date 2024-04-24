6ec1f99d44d784ac56529715c4a4f4d48d4a862f
Gio-Benitez <giobenitez66@gmail.com>
2024-04-23 20:28:33 -0400

    Added Bar chart to dashboard panel

--- frontend/src/lib/components/DashboardPanel.svelte
+++ frontend/src/lib/components/DashboardPanel.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
     import { dashboardStore } from "$lib/stores";
+    import { Bar } from 'svelte-chartjs';
+    import { data } from '$lib/data/test_data_chart'
+
+    import {
+        Chart,
+        Title,
+        Tooltip,
+        Legend,
+        BarElement,
+        CategoryScale,
+        LinearScale,
+    } from 'chart.js';
+
+    Chart.register(
+        Title,
+        Tooltip,
+        Legend,
+        BarElement,
+        CategoryScale,
+        LinearScale
+    );
 </script>
 
-<div class="rounded-2xl aspect-auto w-full h-full bg-slate-800 shadow-lg">
-
+<div class="rounded-2xl aspect-auto py-12 px-12 w-full h-full bg-neutral shadow-lg">
+    <Bar {data} options={{ responsive: true }} />
 </div>
 
 
