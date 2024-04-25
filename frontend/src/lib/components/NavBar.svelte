a91fbf985f5a8b8524c35cfba15cac3fd28d519d
Gio-Benitez <giobenitez66@gmail.com>
2024-04-24 23:38:36 -0400

    Added list of themes to navbar component and refactored the theme selector with Svelte {#each} logic

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -4,8 +4,26 @@
   import AccountBubble from './AccountBubble.svelte';
   import theme_icon from '$lib/images/theme_icon.png';
   import { page } from '$app/stores';
+  
   // let signedIn = false;
   // initialize theme change
+  // Theme array
+  export const themes = [
+      'light',
+      'dark',
+      'dim',
+      'cupcake',
+      'retro',
+      'business',
+      'cyberpunk',
+      'aqua',
+      'valentine',
+      'coffee',
+      'forest',
+      'night',
+      'autumn',
+      'corporate'
+    ];
   onMount(() => {
     themeChange(false);
   });
@@ -35,18 +53,10 @@
             <img alt="Theme Icon" src="{theme_icon}" />
           </div>
         </div>
-        <ul tabindex="0" class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
-          <li><input  data-set-theme="dark" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Dark" value="dark"/></li>
-          <li><input data-set-theme="night" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Night" value="night"/></li>
-          <li><input data-set-theme="light" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Light" value="light"/></li>
-          <li><input data-set-theme="retro" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Retro" value="retro"/></li>
-          <li><input data-set-theme="cyberpunk" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Cyberpunk" value="cyberpunk"/></li>
-          <li><input data-set-theme="valentine" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Valentine" value="valentine"/></li>
-          <li><input data-set-theme="aqua" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Aqua" value="aqua"/></li>
-          <li><input data-set-theme="autumn" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Autumn" value="autumn"/></li>
-          <li><input data-set-theme="coffee" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Coffee" value="coffee"/></li>
-          <li><input data-set-theme="forest" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Forest" value="forest"/></li>
-        </ul>
+        <ul class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+          {#each themes as theme}
+            <li><input data-set-theme="{theme}" data-act-class="ACTIVECLASS" type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="{theme}" value="{theme}"/></li>
+          {/each} 
       </section>
       <AccountBubble />
     </section>
