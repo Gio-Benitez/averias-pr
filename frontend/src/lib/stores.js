4e803c9091926747e679339235d21da6516698af
Gio-Benitez <giobenitez66@gmail.com>
2024-04-25 18:07:55 -0400

    Store rename

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -11,7 +11,7 @@ export let invalidAuth = writable(false);
 // Initialize Map data panel with default values (Stats for all of Puerto Rico)
 export let mapDataStore = writable({
     dataRegion: 'Puerto Rico',
-    numOfReports: 0,
+    numOfReports: 110,
     population: 3205691,
     reportCategory: 'Carretera Rota',
     resolved: 0
@@ -29,7 +29,7 @@ export let dashboardStore = writable({
 
 // Filter Stores
 export const reportCategories = readable({
-    categoryArray: ['All', 'Acera Rota', 'Alcantarilla', 'Carretera Rota', 'Poste de Luz']
+    categoryArray: ['Todas', 'Acera Rota', 'Alcantarilla', 'Carretera Rota', 'Poste de Luz']
 });
 export const municipios = readable({
     municipiosArray: ['Adjuntas', 'Aguada', 'Aguadilla', 'Aguas Buenas', 'Aibonito', 'Añasco', 'Arecibo', 'Arroyo', 'Barceloneta', 'Barranquitas', 'Bayamón', 'Cabo Rojo', 'Caguas', 'Camuy', 'Canóvanas', 'Carolina', 'Cataño', 'Cayey', 'Ceiba', 'Ciales', 'Cidra', 'Coamo', 'Comerío', 'Corozal', 'Culebra', 'Dorado', 'Fajardo', 'Florida', 'Guánica', 'Guayama', 'Guayanilla', 'Guaynabo', 'Gurabo', 'Hatillo', 'Hormigueros', 'Humacao', 'Isabela', 'Jayuya', 'Juana Díaz', 'Juncos', 'Lajas', 'Lares', 'Las Marías', 'Las Piedras', 'Loíza', 'Luquillo', 'Manatí', 'Maricao', 'Maunabo', 'Mayagüez', 'Moca', 'Morovis', 'Naguabo', 'Naranjito', 'Orocovis', 'Patillas', 'Peñuelas', 'Ponce', 'Quebradillas', 'Rincón', 'Río Grande', 'Sabana Grande', 'Salinas', 'San Germán', 'San Juan', 'San Lorenzo', 'San Sebastián', 'Santa Isabel', 'Toa Alta', 'Toa Baja', 'Trujillo Alto', 'Utuado', 'Vega Alta', 'Vega Baja', 'Vieques', 'Villalba', 'Yabucoa', 'Yauco']
