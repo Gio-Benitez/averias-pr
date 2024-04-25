12a465f638f8d7c7b7553019ad16c1443a5d5f46
Gio-Benitez <giobenitez66@gmail.com>
2024-04-24 23:35:42 -0400

    Redesigned map data panel to show statistics clearly and only allow selection of report category

--- frontend/src/lib/components/MapDataPane.svelte
+++ frontend/src/lib/components/MapDataPane.svelte
@@ -15,16 +15,11 @@
   }
 
 </script>
-<div class="flex flex-col flex-wrap w-full h-full bg-primary px-8 py-12 rounded-2xl">
-    <div class="flex flex-col w-full h-2/5 text-center gap-1">
+<div class="flex flex-col flex-wrap gap-4 w-full h-full bg-primary px-8 py-8 rounded-2xl">
+    <div class="flex flex-col w-full h-1/5 text-center">
         <h1>{$mapDataStore.dataRegion}</h1>
         <div class="divider divider-neutral"></div>
-        <select class="select select-secondary w-full">
-            <option selected>{$mapDataStore.dataRegion}</option>
-            {#each municipalities as municipio}
-            <option>{municipio}</option>
-            {/each}    
-        </select>
+        
         <select class="select text-lg font-medium select-secondary w-full">
             <option disabled selected >Seleccione Categoría</option>
             {#each categoryArray as category}
@@ -32,29 +27,30 @@
             {/each}    
         </select>
     </div>
-    <div class="flex flex-col w-full h-1/5 text-center">
+    <div class="flex flex-col w-full text-center">
         <h1>Statistics</h1>
         <div class="divider divider-neutral"></div>
     </div>
-    <div class="flex flex-col flex-wrap w-full h-2/5 text-center gap-2">
-        <div class="stat w-1/2 shadow-lg">
-            <div class="stat-title"># of Reports</div>
-            <div class="stat-value">{$mapDataStore.numOfReports}</div>
-        </div>
-        <div class="stat w-1/2 shadow-lg">
-            <div class="stat-title">Population</div>
-            <div class="stat-value">{$mapDataStore.population}</div>
-        </div>
-        <div class="stat w-1/2 shadow-lg">
-            <div class="stat-title">Report Category</div>
-            <div class="stat-value">{$mapDataStore.reportCategory}</div>
-        </div> 
-        <div class="stat w-1/2 shadow-lg">
-            <div class="stat-title">Stat 2</div>
-            <div class="stat-value">{$mapDataStore.stat2}</div>
+    <div class="flex flex-col flex-wrap w-full h-3/5 text-center gap-2">
+        <div class="stats stats-vertical shadow">
+            <div class="stat shadow-lg">
+                <div class="stat-title">Población</div>
+                <div class="stat-value">{$mapDataStore.population}</div>
+            </div>
+            <div class="stat shadow-lg">
+                <div class="stat-title">Averías Reportadas</div>
+                <div class="stat-value">{$mapDataStore.numOfReports}</div>
+            </div>
+            <div class="stat shadow-lg">
+                <div class="stat-title">Categoría más Común</div>
+                <div class="stat-value">{$mapDataStore.reportCategory}</div>
+            </div>
+            <div class="stat shadow-lg">
+                <div class="stat-title">Averías Resueltas</div>
+                <div class="stat-value">{$mapDataStore.resolved}</div>
+            </div>
         </div>
     </div>
-    
 </div>
 
 <style lang="postcss">
@@ -62,13 +58,13 @@
         @apply text-primary-content text-3xl font-bold;
     }
     .stat-title {
-        @apply text-primary-content text-lg;
+        @apply text-lg;
     }
     .stat-value {
-        @apply text-primary-content text-2xl;
+        @apply text-2xl;
     }
     option {
-        @apply text-base font-medium;
+        @apply font-medium;
     }
     select {
         @apply text-lg font-medium;
