a376cc78a5a5ca108600acea00a9144544cc43d5
Gio-Benitez <giobenitez66@gmail.com>
2024-04-27 17:10:00 -0400

    Testing using chart.js instead of svelte wrapper for chart component

--- frontend/src/lib/components/DashboardPanel.svelte
+++ frontend/src/lib/components/DashboardPanel.svelte
@@ -2,17 +2,9 @@
     import { dashboardStore } from "$lib/stores";
     import { Bar } from 'svelte-chartjs';
     import { data } from '$lib/data/test_data_chart'
+    import { Chart } from 'chart.js/auto';
 
-    import {
-        Chart,
-        Title,
-        Tooltip,
-        Legend,
-        BarElement,
-        CategoryScale,
-        LinearScale,
-    } from 'chart.js';
-
+    /*
     Chart.register(
         Title,
         Tooltip,
@@ -21,10 +13,12 @@
         CategoryScale,
         LinearScale
     );
+    */
+    
 </script>
 
-<div class="rounded-2xl aspect-auto py-12 px-12 w-full h-full bg-neutral shadow-lg">
-    <Bar {data} options={{ responsive: true }} />
+<div class="rounded-2xl aspect-auto w-full h-full bg-primary shadow-lg">
+    <canvas id="testChart"></canvas>
 </div>
 
 
