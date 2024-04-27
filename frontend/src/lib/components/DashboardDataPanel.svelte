bbeca3f7060c8d5ab54b40e7aeb3796e889c854f
Gio-Benitez <giobenitez66@gmail.com>
2024-04-27 17:09:26 -0400

    Implemented form controls to dynamically update filters based on selection

--- frontend/src/lib/components/DashboardDataPanel.svelte
+++ frontend/src/lib/components/DashboardDataPanel.svelte
@@ -1,35 +1,58 @@
 <script lang="ts">
+  import type { EFC_148892 } from "$env/static/private";
     import { reportCategories, municipios, dashboardStore } from "$lib/stores";
     import { createEventDispatcher } from "svelte";
     
     // Report Category Filter Logic
-    let catUnselected: boolean = true;
-    let muniUnselected: boolean = true;
+    let selected;
     let cat: string = '';
-    let muni: string = '';
-    const municipalities = $municipios.municipiosArray;
+    let options: string = '';
+    const municipalities = $municipios.municipiosList;
     const categoryArray = $reportCategories.categoryArray;
+    const reportAxis = [{axis_id: 1, option: 'number'}, {axis_id: 2, option: 'percentage'}];
+    console.log(cat);
     function updateChart (data: any) {
         $dashboardStore.chartData = data;
         console.log($dashboardStore.chartData);
-  }
+    }
+    function handleSubmit() {
+		alert(`Selected option ${selected.name} (${selected})"`);
+	}
 
 </script>
 <div class="flex flex-col flex-wrap gap-4 w-full h-full bg-primary px-8 py-8 rounded-2xl">
     <div class="flex flex-col w-full h-1/5 text-center">
+
         <h1>Variables Requeridas</h1>
         <div class="divider divider-neutral"></div>
-        <h2>Variable 1 (Eje Vertical)</h2>
-        <select class="select text-lg font-medium select-secondary w-full">
-            <option selected ># de Reportes</option>
-            <option>% de Reportes</option>  
-        </select>
+
+        <!--Form section to handle dynamic filter options for Dashboard Panel Parameters-->
+        <label for="variable-1">Variable 1 (Eje Vertical)</label>
+        <form on:submit|preventDefault={handleSubmit}>
+            <select id="variable-1" bind:value={selected}>
+                <option value='number'># de Reportes</option>
+                <option value='nercentage'>% de Reportes</option>  
+            </select>
+            <select bind:value={selected}>
+                {#each municipalities as municipio}
+                    <option value={municipio}>{municipio.name}</option>
+                {/each}
+            </select>
+            <button class="btn btn-primary">Submit</button>
+        </form>
+
+        
         <h2>Variable 2 (Eje Horizontal)</h2>
-        <select class="select text-lg font-medium select-secondary w-full">
+        <select bind:value={selected} >
+            <option selected value='Municipio'>Municipios</option>
+            <option value='Categoría'>Categorías</option> 
+        </select>
+
+        <h2>Seleccione Municipios</h2>
+        <select >
             <option selected>Municipios</option>
             <option>Categorías</option> 
         </select>
-        
         <div class="divider divider-neutral"></div>
         <h1>Variables Opcionales</h1>
             
@@ -50,6 +73,6 @@
         @apply font-medium;
     }
     select {
-        @apply text-lg font-medium;
+        @apply select text-lg font-medium select-secondary w-full;
     }
 </style>
