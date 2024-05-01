e5284656140a042785d0fdab008942ffbea60a0a
Gio-Benitez <giobenitez66@gmail.com>
2024-05-01 15:39:49 -0400

    Form actions testing with external code

--- frontend/src/routes/chart/+page.svelte
+++ frontend/src/routes/chart/+page.svelte
@@ -1,9 +1,53 @@
-<script lang="ts">
-	import TestChart from '$components/TestChart.svelte';
+<script>
+    export let data;
+    import { municipios } from '$lib/stores.js';
+
+    let selectedUser = '';
+    let selectedAsset = '';
+
+    function handleUserChange(event) {
+        selectedUser = event.target.value;
+        selectedAsset = ''; // Reset selected course when user changes
+    }
+
+    function handleCourseChange(event) {
+        selectedAsset = event.target.value;
+    }
+
+    async function showSelection() {
+        console.log('Selected User:', selectedUser);
+        console.log('Selected Course:', selectedAsset);
+    }
 </script>
 
-<main class="grid grid-rows-10 grid-cols-10 h-full w-full gap-8 pb-8">
-    <div id="chart" class="col-start-2 col-end-8 row-start-2 row-end-10">
-        <TestChart />
-    </div>
-</main>
+<div>
+    {#if data}
+        <form action="?/userSelection" method="POST">
+            <select
+                class="select select-accent w-full max-w-xs"
+                value={selectedUser}
+                on:change={handleUserChange}
+            >
+                <option disabled selected>Select a User</option>
+                {#each $municipios.municipiosList as user}
+                    <option name="user" value={user.muni_id}>{user.muni_id}</option>
+                {/each}
+            </select>
+            {#if selectedUser !== ''}
+                <select
+                    class="select select-accent w-full max-w-xs"
+                    value={selectedAsset}
+                    on:change={handleCourseChange}
+                >
+                    <option disabled selected>Select a Course</option>
+                    {#each $municipios.municipiosList.find((user) => user.name === selectedUser)?.name || [] as course}
+                        <option name="course" value={course}>{course}</option>
+                    {/each}
+                </select>
+            {/if}
+            <button type="button" on:click={showSelection}>Get Data</button>
+        </form>
+    {:else}
+        <p>Loading...</p>
+    {/if}
+</div>
