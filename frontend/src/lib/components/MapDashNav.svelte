2c560da1a0e267418ad9b302061b43a06374393f
Gio-Benitez <giobenitez66@gmail.com>
2024-05-02 04:11:21 -0400

    Changed styling on Map and Dashboard navigation tabs

--- frontend/src/lib/components/MapDashNav.svelte
+++ frontend/src/lib/components/MapDashNav.svelte
@@ -1,4 +1,4 @@
-<div class="flex pt-12 pl-0 space-x-2 col-span-3 row-span-1 col-start-2 row-start-1 ">
+<div class="flex flex-nowrap flex-col col-start-2 space-x-2">
     <div class="flex h-full ">
         <div class="tabs-lifted tabs-lg w-full h-full">
             <a role="tab" href="/map" class="tab">Interactive Map</a>
