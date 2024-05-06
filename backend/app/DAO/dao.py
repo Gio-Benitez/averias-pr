081b92df53a742db2df4d3362e1f9e3128e1f909
Gio-Benitez <giobenitez66@gmail.com>
2024-05-06 02:53:32 -0400

    Added close and rollback methods, included commit in execute query method until future refactor of routes

--- backend/app/DAO/dao.py
+++ backend/app/DAO/dao.py
@@ -8,7 +8,14 @@ class BaseDAO:
             cur.execute(query, params)
         else:
             cur.execute(query)
+        self.conn.commit()
         return cur
 
     def commit(self):
         self.conn.commit()
+
+    def rollback(self):
+        self.conn.rollback()
+    
+    def close(self):
+        self.conn.close()
