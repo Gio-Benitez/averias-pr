d851a94e6eacff9d6412abb45e3fb89a0f0c0f2a
Gio-Benitez <giobenitez66@gmail.com>
2024-05-09 22:10:43 -0400

    Added reset button for stat panel, stat sizing changes

--- frontend/src/routes/map/+page.svelte
+++ frontend/src/routes/map/+page.svelte
@@ -27,6 +27,11 @@
     function updateWithForm (event: any) {
         $mapDataStore.dataRegion = form?.props.mapData.dataRegion as string;
     } 
+    function panelReset () {
+        $mapDataStore.dataRegion = "Puerto Rico";
+        catSelected = false;
+        category = "";
+    }
     // Dashboard Data Panel Logic
     const municipalities = $municipios.municipiosList;
   </script>
@@ -51,58 +56,60 @@
                 <h1>{$mapDataStore.dataRegion}</h1>
                 
                 <div class="divider divider-neutral"></div>
-                    <select name="category" on:change={selectCat} class="select text-lg font-medium select-secondary w-full">
-                        <option disabled selected >Seleccione Categoría</option>
-                        {#each $filterCategoriesStore as category}
-                        <option value={category}>{category}</option>
-                        {/each}    
-                    </select>
-                    <!-- Submit Button -->
-                    
+                    <div class="flex items-center flex-col gap-2">
+                        <select name="category" on:change={selectCat} class="select text-lg font-medium select-secondary w-full">
+                            <option disabled selected >Seleccione Categoría</option>
+                            {#each $filterCategoriesStore as category}
+                            <option value={category}>{category}</option>
+                            {/each}    
+                        </select>
+                        <button name="reset" on:click={panelReset}>Restaurar</button>
+                        
+                    </div>
             </div>
             <div class="flex flex-col w-full text-center">
                 <h1>Statistics</h1>
                 <div class="divider divider-neutral"></div>
             </div>
             {#if !catSelected}
-            <div class="flex flex-col flex-wrap w-full text-center gap-0">
+            <div class="flex flex-col flex-wrap text-center gap-0">
                 <div class="stats stats-vertical shadow">
                     <div class="stat shadow-lg">
-                        <div class="stat-title">Población</div>
-                        <div class="stat-value">{mapStats[$mapDataStore.dataRegion].population}</div>
-                    </div>
+                        <div class="stat-title   lg:text-lg">Población</div>
+                        <div class="stat-value   lg:text-xl">{mapStats[$mapDataStore.dataRegion].population}</div>
+                    </div>  
                     <div class="stat shadow-lg">
-                        <div class="stat-title">Categoría más Común</div>
-                        <div class="stat-value">{mapStats[$mapDataStore.dataRegion].most_common_category}</div>
-                    </div>
+                        <div class="stat-title  lg:text-lg">Categoría más Común</div>
+                        <div class="stat-value  lg:text-xl">{mapStats[$mapDataStore.dataRegion].most_common_category}</div>
+                    </div>  
                     <div class="stat shadow-lg">
-                        <div class="stat-title">Averías Reportadas</div>
-                        <div class="stat-value">{mapStats[$mapDataStore.dataRegion].num_reports}</div>
-                    </div>
+                        <div class="stat-title  lg:text-lg">Averías Reportadas</div>
+                        <div class="stat-value  lg:text-xl">{mapStats[$mapDataStore.dataRegion].num_reports}</div>
+                    </div>  
                     <div class="stat shadow-lg">
-                        <div class="stat-title">Averías Resueltas</div>
-                        <div class="stat-value">{mapStats[$mapDataStore.dataRegion].resolved_reports}</div>
+                        <div class="stat-title  lg:text-lg">Averías Resueltas</div>
+                        <div class="stat-value  lg:text-xl">{mapStats[$mapDataStore.dataRegion].resolved_reports}</div>
                     </div>
                 </div>
             </div>
             {:else if catSelected}
-            <div class="flex flex-col flex-wrap w-full text-center gap-0">
+            <div class="flex flex-col flex-wrap w-min text-center gap-0">
                 <div class="stats stats-vertical shadow">
                     <div class="stat shadow-lg">
-                        <div class="stat-title">Población</div>
-                        <div class="stat-value">{mapStats[$mapDataStore.dataRegion].population}</div>
+                        <div class="stat-title lg:text-lg">Población</div>
+                        <div class="stat-value lg:text-xl">{mapStats[$mapDataStore.dataRegion].population}</div>
                     </div>
                     <div class="stat shadow-lg">
-                        <div class="stat-title">Categoría Seleccionada</div>
-                        <div class="stat-value">{category}</div>
+                        <div class="stat-title lg:text-lg">Categoría Seleccionada</div>
+                        <div class="stat-value lg:text-xl">{category}</div>
                     </div>
                     <div class="stat shadow-lg">
-                        <div class="stat-title">Averías Reportadas</div>
-                        <div class="stat-value">{mapStats[$mapDataStore.dataRegion].categories[category].total}</div>
+                        <div class="stat-title lg:text-lg">Averías Reportadas</div>
+                        <div class="stat-value lg:text-xl">{mapStats[$mapDataStore.dataRegion].categories[category].total}</div>
                     </div>
-                    <div class="stat shadow-lg">
-                        <div class="stat-title">Averías Resueltas</div>
-                        <div class="stat-value">{mapStats[$mapDataStore.dataRegion].categories[category].resolved}</div>
+                    <div class="stat shadow-lg ">
+                        <div class="stat-title lg:text-lg">Averías Resueltas</div>
+                        <div class="stat-value lg:text-xl">{mapStats[$mapDataStore.dataRegion].categories[category].resolved}</div>
                     </div>
                 </div>
             </div>
@@ -117,7 +124,7 @@
         overflow: scroll;
     }
     button {
-        @apply  btn-md text-primary font-medium text-lg;
+        @apply  btn-md bg-primary-content rounded-lg w-1/2 text-primary  font-medium text-lg;
     }
     h1 {
         @apply text-primary-content text-3xl font-bold;
@@ -126,16 +133,19 @@
         @apply text-primary-content text-lg font-medium;
     }
     .stat-title {
-        @apply text-lg font-semibold;
+        @apply font-semibold text-base;
     }
     .stat-value {
-        @apply text-xl font-bold;
+        @apply font-bold text-lg;
     }
     option {
         @apply font-medium;
     }
     select {
         @apply select select-secondary w-full text-lg font-medium;
+    }
+    .stats {
+        @apply w-min;
     }
     
 </style>
