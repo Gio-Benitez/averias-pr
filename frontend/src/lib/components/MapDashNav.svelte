bf24d6eae0246ee89ba9a6fcfd3f0063937e7fd9
Gio-Benitez <giobenitez66@gmail.com>
2024-05-10 21:36:42 -0400

    Removed dashboard tab

--- frontend/src/lib/components/MapDashNav.svelte
+++ frontend/src/lib/components/MapDashNav.svelte
@@ -2,7 +2,7 @@
     <div class="flex h-full ">
         <div class="tabs-lifted tabs-lg w-full h-full">
             <a role="tab" href="/map" class="tab">Interactive Map</a>
-            <a role="tab" href="/dashboard" class="tab">Dashboard</a>
+            <!-- <a role="tab" href="/dashboard" class="tab">Dashboard</a> -->
         </div>
     </div>
 </div>
